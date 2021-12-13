b392bc223be814094fcabbdce764b6ead1611a90
Елизавета <@DESKTOP-4BO8U2C>
2021-12-13 19:34:39 +0800

    Добавьте файлы проекта.

---
+++ example/example.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    cout << "введите натуральное число: ";
+    int N, Sum = 0, Sum_m = 0, osn;
+    cin >> N;
+    for (int i = 2; i <= 9; i++) {
+        int Ncopy = N;
+        Sum = 0;
+        while (Ncopy > 0) {
+            Sum += Ncopy % i;
+            Ncopy /= i;
+        }
+        if (Sum > Sum_m) {
+            Sum_m = Sum;
+            osn = i;
+        }
+    }
+    cout << "наибольшая сумма чисел в основании системы " << osn <</*" "<<Sum_m<<*/ endl;
+
+    for (int i = 2; i <= 9; i++) {
+        int Ncopy = N;
+        Sum = 0;
+        while (Ncopy > 0) {
+            Sum += Ncopy % i;
+            Ncopy /= i;
+        }
+        if (Sum ==
+            Sum_m) {
+            cout << i << endl;
+        }
+    }
+}
